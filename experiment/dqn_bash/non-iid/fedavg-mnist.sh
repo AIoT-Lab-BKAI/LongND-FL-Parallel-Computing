52cd2fb7db0aa8fedad2e890f66d99ad54c6dc21
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-01 21:46:17 +0700

    fix add log

--- experiment/dqn_bash/non-iid/fedavg-mnist.sh
+++ experiment/dqn_bash/non-iid/fedavg-mnist.sh
@@ -1,1 +1,1 @@
-python3 -u train_SGD.py --log_file='fedavg-mnist-non-iid' --algorithm='fedavg' --num_clients=10 --clients_per_round=10 --num_rounds=100 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --num_samples_per_class=980 --log_file='log/non-iid/fedavg'
+python3 -u train_SGD.py --log_file='fedavg-mnist-non-iid' --algorithm='fedavg' --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --num_samples_per_class=980 --log_file='log/non-iid/fedavg'
