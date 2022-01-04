c20d8a74d98ea1b17fdca708d82cd8f6f6fdf7fb
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-04 12:08:06 +0700

    change logging

--- experiment/dqn_bash/non-iid/fedprox-mnist.sh
+++ experiment/dqn_bash/non-iid/fedprox-mnist.sh
@@ -1,1 +1,1 @@
-python3 -u train_SGD.py --log_file='fedprox-mnist-non-iid' --algorithm='fedprox' --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --num_samples_per_class=980 --mu=0.1 --log_file='log/non-iid/fedprox'
+python3 -u train_SGD.py --log_file='fedprox-mnist-non-iid' --algorithm='fedprox' --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --num_samples_per_class=980 --mu=0.1 --log_file='log/noniid/fedprox'
