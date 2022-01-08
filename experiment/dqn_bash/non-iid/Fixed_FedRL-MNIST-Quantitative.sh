62e2be8e8c2fa56f1143b00a2c49a98ed16ba2f7
thanhhff <minhhieun4@gmail.com>
2022-01-08 18:31:49 +0700

    update script

--- experiment/dqn_bash/non-iid/Fixed_FedRL-MNIST-Quantitative.sh
+++ experiment/dqn_bash/non-iid/Fixed_FedRL-MNIST-Quantitative.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=3 python3 -u train_SGD.py --num_clients=10 --clients_per_round=10 --num_rounds=2000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/MNIST-quantitative_non-iid.txt" --run_name="FedRL-MNIST-Quantitative" --group_name="Fixed Epochs"
+CUDA_VISIBLE_DEVICES=1 python3 -u train_SGD_fixed.py --num_clients=10 --clients_per_round=10 --num_rounds=2000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/MNIST-quantitative_non-iid.txt" --run_name="FedRL-MNIST-Quantitative" --group_name="Fixed Epochs"
 
