3686d416c488360d28ae709684ac3fe551ea2efb
Viet Hoang <vietyb00@outlook.com>
2022-01-10 11:54:06 +0900

    Fix bug plot in utils file

--- experiment/benchmark/non-iid/FedProx-MNIST-Featured.sh
+++ experiment/benchmark/non-iid/FedProx-MNIST-Featured.sh
@@ -1,2 +1,12 @@
-python3 -u train_benchmark.py --num_clients=10 --clients_per_round=10 --num_rounds=2000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/MNIST-featured_non-iid.txt" --run_name="FedProx-MNIST-Featured" --group_name="Benchmark" --algorithm="fedprox" --mu=0.01
+#!/bin/bash
+#$-l rt_G.small=1
+#$-l h_rt=36:00:00
+#$-j y
+#$-cwd
+source /etc/profile.d/modules.sh
+module load gcc/9.3.0 python/3.8/3.8.7 cuda/11.0/11.0.3 cudnn/8.0/8.0.5
+source /home/acc13085dy/federated-learning/FLenv/bin/activate
+cd /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing
+
+python3 -u train_benchmark.py --num_clients=10 --clients_per_round=10 --num_rounds=2000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/MNIST-featured_non-iid.txt" --run_name="Benchmark-FedProx-MNIST-Featured" --group_name="Benchmark" --algorithm="fedprox" --mu=0.01
 
