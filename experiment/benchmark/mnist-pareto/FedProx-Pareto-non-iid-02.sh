fc8e4097fb08d74959f3f01f6aeca465921aa6ec
Viet Hoang <vietyb00@outlook.com>
2022-01-12 02:01:44 +0900

    Update run script on ABCI Server

--- experiment/benchmark/mnist-pareto/FedProx-Pareto-non-iid-02.sh
+++ experiment/benchmark/mnist-pareto/FedProx-Pareto-non-iid-02.sh
@@ -10,5 +10,5 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train_benchmark.py --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/mnist/equal/MNIST-noniid-fedavg_pareto_2.json" --algorithm="fedprox" --run_name="FedProx-Pareto-Non-IID-02" --group_name="MNIST-Pareto" --mu=0.01
+python3 -u train_benchmark.py --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/mnist/pareto/MNIST-noniid-fedavg_pareto_2.json" --algorithm="fedprox" --run_name="FedProx-Pareto-Non-IID-02" --group_name="MNIST-Pareto" --mu=0.01
 
