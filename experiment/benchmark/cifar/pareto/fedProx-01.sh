542b0f108edb0f5b98c5a28dace5a69985591cba
Viet Hoang <vietyb00@outlook.com>
2022-01-12 19:27:13 +0900

    Update script run cifar

--- experiment/benchmark/cifar/pareto/fedProx-01.sh
+++ experiment/benchmark/cifar/pareto/fedProx-01.sh
@@ -10,5 +10,5 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train.py --train_mode="benchmark" --dataset_name="cifar" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/pareto/CIFAR-noniid-fedavg_pareto_1.json" --algorithm="FedProx" --mu=0.01 --run_name="FedProx-Pareto-Non-IID-01" --group_name="CIFAR-Pareto"
+python3 -u train.py --train_mode="benchmark" --dataset_name="cifar100" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/pareto/CIFAR-noniid-fedavg_pareto_1.json" --algorithm="FedProx" --mu=0.01 --run_name="FedProx-Pareto-Non-IID-01" --group_name="CIFAR-Pareto"
 
