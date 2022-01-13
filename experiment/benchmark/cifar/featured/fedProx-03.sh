13283d83ea417e74fd1b16af6fd26c2f80c3b48e
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-13 17:07:46 +0700

    fix path

--- experiment/benchmark/cifar/featured/fedProx-03.sh
+++ experiment/benchmark/cifar/featured/fedProx-03.sh
@@ -10,4 +10,4 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train.py --train_mode="benchmark" --dataset_name="cifar100" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/featured/CIFAR-noniid-fedavg_featured_3.json" --algorithm="FedProx" --mu=0.01 --run_name="FedProx-Featured-Non-IID-03" --group_name="CIFAR-Featured"
+python3 -u train.py --train_mode="benchmark" --dataset_name="cifar100" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/featured/CIFAR-noniid-featured_3.json" --algorithm="FedProx" --mu=0.01 --run_name="FedProx-Featured-Non-IID-03" --group_name="CIFAR-Featured"
