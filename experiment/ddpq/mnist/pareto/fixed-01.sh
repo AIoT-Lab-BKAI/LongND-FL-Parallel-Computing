3876a182c37fb6e7aacfdedc36ae3cf6f38073f5
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-13 18:53:03 +0700

    for testing only

--- experiment/ddpq/mnist/pareto/fixed-01.sh
+++ experiment/ddpq/mnist/pareto/fixed-01.sh
@@ -10,5 +10,5 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train.py --train_mode="RL-Fixed" --dataset_name="mnist" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/mnist/pareto/MNIST-noniid-fedavg_pareto_1.json" --run_name="FedRL-Fixed-Pareto-Non-IID-01" --group_name="MNIST-Pareto"
+python3 -u train.py --train_mode="RL-Fixed" --dataset_name="mnist" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/mnist/pareto/MNIST-noniid-fedavg_pareto_1.json" --run_name="FedRL-Fixed-Pareto-Non-IID-01" --group_name="MNIST-Pareto-dung"
 
