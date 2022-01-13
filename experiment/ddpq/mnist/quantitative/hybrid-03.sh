55d0be012f38cbec539c7fc444f8a46d5faa34a2
thuydung-icthust <minhhieun4@gmail.com>
2022-01-13 15:19:07 +0700

    Update script MNIST quantitative

---
+++ experiment/ddpq/mnist/quantitative/hybrid-03.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$-l rt_G.small=1
+#$-l h_rt=36:00:00
+#$-j y
+#$-cwd
+source /etc/profile.d/modules.sh
+module load gcc/9.3.0 python/3.8/3.8.7 cuda/11.1/11.1.1 cudnn/8.0/8.0.5
+source /home/acc13085dy/federated-learning/FLenv/bin/activate
+
+cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
+cd $SGE_LOCALDIR/$JOB_ID
+
+python3 -u train.py --train_mode="RL-Hybrid" --dataset_name="mnist" --num_clients=10 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/mnist/quantitative/MNIST-noniid-quantitative_3.json" --run_name="FedRL-Hybrid-Quantitative-Non-IID-03" --group_name="MNIST-Quantitative"
+
