2d95530cf4416fece1330628a92100caf1e3aa6a
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-19 22:29:20 +0700

    first commit for ver 2.5.1.3

--- ddpg_agent/utils.py
+++ ddpg_agent/utils.py
@@ -47,7 +47,7 @@ def get_reward(losses, beta=0.45):
     # beta = 0.45
     losses = np.asarray(losses)
     # return - beta * np.mean(losses) - (1 - beta) * np.std(losses)
-    return - np.mean(losses) - np.std(losses)
+    return - np.mean(losses) - (losses.max() - losses.min())
 
 
 def get_info_from_dqn_weights(weights, num_clients, dqn_list_epochs):
