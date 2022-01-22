2335678d495b0ea563e96cdefe7b8708a4fca7ae
duclong1009 <duclong10092000@gmail.com>
2022-01-22 07:30:14 +0700

    train upperbound

---
+++ experiment/test2.sh
@@ -0,0 +1,3 @@
+
+python3 -u train_upperbound.py --num_rounds=100 --learning_rate=0.01 --batch_size=16 --num_epochs=1 --dataset_name="mnist" --path_data_idx="dataset_idx/mnist/100client/equal/MNIST-noniid-fedavg_equal_100.json" --run_name="FedRL-equal-01" --group_name="Upgrade-100"
+
