9ceecdf315bc66648fd9a3087757b3ae1635b7f9
thuydung-icthust <minhhieun4@gmail.com>
2022-01-23 21:05:36 +0700

    Update run script level 100

---
+++ abci/MNIST-LEVEL-100.sh
@@ -0,0 +1,49 @@
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Featured/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Featured/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Featured/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Featured/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Quantitative/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Quantitative/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Quantitative/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/10/Quantitative/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Featured/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Featured/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Featured/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Featured/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Quantitative/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Quantitative/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Quantitative/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/20/Quantitative/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Featured/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Featured/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Featured/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Featured/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Quantitative/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Quantitative/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Quantitative/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/30/Quantitative/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Featured/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Featured/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Featured/fixed-100.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Quantitative/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Quantitative/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/40/Quantitative/fixed-100.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Featured/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Featured/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Featured/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Featured/Upperbound.sh
+
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Quantitative/fedAVG-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Quantitative/fedProx-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Quantitative/fixed-100.sh
+qsub -g gad50699 ../experiment/FMNIST-100-10-01/50/Quantitative/Upperbound.sh
+
