3360b8b3b6f950d42df30ac28fee73cd85cd87c0
thuydung-icthust <minhhieun4@gmail.com>
2022-01-23 16:37:51 +0700

    CIFAR-100

--- experiment/CIFAR-100-01/Main/Featured/fedAVG-100-01.sh
+++ experiment/CIFAR-100-01/Main/Featured/fedAVG-100-01.sh
@@ -10,5 +10,4 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train.py --train_mode="RL-Fixed" --dataset_name="cifar100" --num_clients=100 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/100client/equal/CIFAR-noniid-fedavg_equal_100.json" --run_name="FedRL-Fixed-100-Clients" --group_name="CIFAR-FedAvg-Equal"
-
+python3 -u train.py --train_mode="benchmark" --dataset_name="cifar100" --num_clients=100 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/100client/featured/60/CIFAR-noniid-featured_60.json" --run_name="FedAVG-Featured-100-01-Main" --group_name="CIFAR-100-10"
