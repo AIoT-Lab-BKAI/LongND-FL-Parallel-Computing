9acd2b097337e8e79c38bb9f48eac70451acede9
thuydung-icthust <minhhieun4@gmail.com>
2022-01-24 10:23:46 +0700

    Fix run name on ABCI Server

--- experiment/CIFAR-100-01/Main/Featured/fedAdp-01.sh
+++ experiment/CIFAR-100-01/Main/Featured/fedAdp-01.sh
@@ -10,4 +10,4 @@ source /home/acc13085dy/federated-learning/FLenv/bin/activate
 cp -rp /home/acc13085dy/federated-learning/LongND-FL-Parallel-Computing $SGE_LOCALDIR/$JOB_ID/
 cd $SGE_LOCALDIR/$JOB_ID
 
-python3 -u train.py --train_mode="fedadp" --dataset_name="cifar100" --num_clients=100 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/100client/featured/60/CIFAR-noniid-featured_60.json" --run_name="fedadp-Featured-01" --group_name="CIFAR-100-10"
+python3 -u train.py --train_mode="fedadp" --dataset_name="cifar100" --num_clients=100 --clients_per_round=10 --num_rounds=1000 --learning_rate=0.01 --batch_size=10 --num_epochs=5 --path_data_idx="dataset_idx/cifar/100client/featured/60/CIFAR-noniid-featured_60.json" --run_name="fedadp-Featured-01-Main" --group_name="CIFAR-100-10"
