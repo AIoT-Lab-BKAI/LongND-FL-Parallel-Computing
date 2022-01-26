41aeaa1200313b482d737223f83760f9f4330ecd
thuydung-icthust <minhhieun4@gmail.com>
2022-01-26 10:11:54 +0700

    Add num_episode into option

--- utils/option.py
+++ utils/option.py
@@ -34,5 +34,6 @@ def option():
     parser.add_argument("--num_core", type=int, default=6)
     parser.add_argument("--save_model", type=int, default=0)
     parser.add_argument("--load_model", type=int, default=0)
+    parser.add_argument("--num_episode", type=int, default=1)
     args = parser.parse_args()
     return args
